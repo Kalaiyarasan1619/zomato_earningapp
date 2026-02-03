94b9a37b46d4fc1d6fa4c05b717e55fa191447bd
Kalaiyarasan1619 <155789550+Kalaiyarasan1619@users.noreply.github.com>
2026-02-03 13:46:41 +0530

    fixed

--- zomato-earning-backend/server.js
+++ zomato-earning-backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
 app.use('/api/earnings', earningsRouter);
 
 export default app;
